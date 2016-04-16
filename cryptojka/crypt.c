811cff00792b61327469d079c88e144f5f38959c
JoseluCross <jlgarrido97@gmail.com>
2016-04-16 16:35:48 +0200

    Update 0.4.8

--- cryptojka/crypt.c
+++ cryptojka/crypt.c
@@ -2,14 +2,14 @@
 
 
 /*
- *Title: crypt
- *Description: It de/encrypt strings
- *@param pass[]: string which we use like password
- *@param text[]: string which we will encrypt
- *@param x: false = encrypt, true = decrypt
- *@param name[]: name of output
- *@param version: true: use the cryptetion before 0.4.5
- */
+*Title: crypt
+*Description: It de/encrypt strings
+*@param pass[]: string which we use like password
+*@param text[]: string which we will encrypt
+*@param x: false = encrypt, true = decrypt
+*@param name[]: name of output
+*@param version: true: use the cryptetion before 0.4.5
+*/
 void crypt(char pass[], char *text, bool x, char name[], bool version) {
   int     i;//Loop number
   int     pass_length;
@@ -28,29 +28,30 @@ void crypt(char pass[], char *text, bool x, char name[], bool version) {
     if(passPosition == pass_length) {
       passPosition = 0;
       if(version==false){
-      for(i=0;i<pass_length;i++){
-        pass[i]++;
-        if(pass[i]>126){
-          pass[i]-=94;
+        for(i=0;i<pass_length;i++){
+          pass[i]++;
+          if(pass[i]>126){
+            pass[i]-=94;
+          }
         }
       }
-    }
     }
     if(x == false) {
       sol = *text + pass[passPosition];
       while(sol > 126) {
-	sol -= 94;
+        sol -= 94;
       }
     } else {
       sol = *text - pass[passPosition];
       while(sol < 32) {
-	sol += 94;
+        sol += 94;
       }
     }
     passPosition++;
+    if(*text!=0){
+      fputc(sol, nom);
+    }
     text++;
-    if(sol!=0)
-    fputc(sol, nom);
   }
   fclose(nom);
 }
